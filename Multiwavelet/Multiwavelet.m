85ab6807102b88ceec7a93f3f9a0621cbe3beb78
Natz12 <43297232+Natz12@users.noreply.github.com>
2019-11-16 17:22:36 -0500

    Multiwavelet style correction

--- Multiwavelet/Multiwavelet.m
+++ Multiwavelet/Multiwavelet.m
@@ -1,126 +1,124 @@
 % Read MRI image and convert it to RGB format %
-[A,map] = imread("MRI.gif",1); 
-A_RGB = ind2rgb(A,map);
+[x1,map] = imread("MRI.gif",1); 
+x1_RGB = ind2rgb(x1,map);
 
 % Read SPECT image and convert it to RGB format %
-[B,map] = imread("SPECT.gif",1);
-B_RGB = ind2rgb(B,map);
+[x2,map] = imread("SPECT.gif",1);
+x2_RGB = ind2rgb(x2,map);
 
 % Perform Single-Level 2D Multiwavelet decomposition on RGB channels of MRI image %  
-X_Red = GHM(A_RGB(:,:,1));
-X_Green = GHM(A_RGB(:,:,2));
-X_Blue = GHM(A_RGB(:,:,3));
+x1_Red = GHM(x1_RGB(:,:,1));
+x1_Green = GHM(x1_RGB(:,:,2));
+x1_Blue = GHM(x1_RGB(:,:,3));
 
 % Perform Single-Level 2D Multiwavelet decomposition on RGB channels of SPECT image % 
-Y_Red = GHM(B_RGB(:,:,1));
-Y_Green = GHM(B_RGB(:,:,2));
-Y_Blue = GHM(B_RGB(:,:,3));
+x2_Red = GHM(x2_RGB(:,:,1));
+x2_Green = GHM(x2_RGB(:,:,2));
+x2_Blue = GHM(x2_RGB(:,:,3));
 
 % Concatenate RGB channels
-X = cat(3,X_Red,X_Green,X_Blue);
-Y = cat(3,Y_Red,Y_Green,Y_Blue);
+x1 = cat(3,x1_Red,x1_Green,x1_Blue);
+x2 = cat(3,x2_Red,x2_Green,x2_Blue);
 
 % Plot LL, LH, HL, and HH frequency sub-bands %
 figure(1);
-imshow(X);
+imshow(x1);
 figure(2);
-imshow(Y);
+imshow(x2);
 
 % Extract LL, LH, HL, and HH frequency sub-bands %
-[N_A,M_A]=size(A_RGB(:,:,1));
-[N_B,M_B]=size(B_RGB(:,:,1));
+[N_x1,M_x1]=size(x1_RGB(:,:,1));
+[N_x2,M_x2]=size(x2_RGB(:,:,1));
 
-LL_X = X(1:N_A, 1:N_A, :);
-LH_X = X(1:N_A, N_A+1:2*N_A, :);
-HL_X = X(N_A+1:2*N_A, 1:N_A, :);
-HH_X = X(N_A+1:2*N_A, N_A+1:2*N_A, :);
+LL_x1 = x1(1:N_x1, 1:N_x1, :);
+LH_x1 = x1(1:N_x1, N_x1+1:2*N_x1, :);
+HL_x1 = x1(N_x1+1:2*N_x1, 1:N_x1, :);
+HH_x1 = x1(N_x1+1:2*N_x1, N_x1+1:2*N_x1, :);
 
-LL_Y = Y(1:N_B, 1:N_B, :);
-LH_Y = Y(1:N_B, N_B+1:2*N_B, :);
-HL_Y = Y(N_B+1:2*N_B, 1:N_B, :);
-HH_Y = Y(N_B+1:2*N_B, N_B+1:2*N_B, :);
+LL_x2 = x2(1:N_x2, 1:N_x2, :);
+LH_x2 = x2(1:N_x2, N_x2+1:2*N_x2, :);
+HL_x2 = x2(N_x2+1:2*N_x2, 1:N_x2, :);
+HH_x2 = x2(N_x2+1:2*N_x2, N_x2+1:2*N_x2, :);
 
 % Average fusion of LL sub-band %
-LL_F = 0.5*(LL_X+LL_Y);
+LL_y = 0.5*(LL_x1+LL_x2);
 
 % Fusion based on salience measure for high frequency sub-bands (suggested by: HAI-HUI WANG)
-LH_F = zeros(size(LH_X,1),size(LH_X,2),3);
-LH_X = padarray(LH_X,[1 1],0,'both');
-LH_Y = padarray(LH_Y,[1 1],0,'both');
+LH_y = zeros(size(LH_x1,1),size(LH_x1,2),3);
+LH_x1 = padarray(LH_x1,[1 1],0,'both');
+LH_x2 = padarray(LH_x2,[1 1],0,'both');
 
 for k=1:3
-    for i=2:size(LH_X,1)-1
-        for j=2:size(LH_X,2)-1
-            S_x = LH_X(i,j,k)+LH_X(i-1,j-1,k)+LH_X(i-1,j,k)+LH_X(i-1,j+1,k)+LH_X(i,j-1,k)+LH_X(i,j+1,k)+LH_X(i+1,j-1,k)+LH_X(i+1,j,k)+LH_X(i+1,j+1,k);
-            S_x = S_x*(LH_X(i,j,k))^2;
+    for i=2:size(LH_x1,1)-1
+        for j=2:size(LH_x1,2)-1
+            S_x = LH_x1(i,j,k)+LH_x1(i-1,j-1,k)+LH_x1(i-1,j,k)+LH_x1(i-1,j+1,k)+LH_x1(i,j-1,k)+LH_x1(i,j+1,k)+LH_x1(i+1,j-1,k)+LH_x1(i+1,j,k)+LH_x1(i+1,j+1,k);
+            S_x = S_x*(LH_x1(i,j,k))^2;
 
-            S_y = LH_Y(i,j,k)+LH_Y(i-1,j-1,k)+LH_Y(i-1,j,k)+LH_Y(i-1,j+1,k)+LH_Y(i,j-1,k)+LH_Y(i,j+1,k)+LH_Y(i+1,j-1,k)+LH_Y(i+1,j,k)+LH_Y(i+1,j+1,k);
-            S_y = S_y*(LH_Y(i,j,k))^2;
+            S_y = LH_x2(i,j,k)+LH_x2(i-1,j-1,k)+LH_x2(i-1,j,k)+LH_x2(i-1,j+1,k)+LH_x2(i,j-1,k)+LH_x2(i,j+1,k)+LH_x2(i+1,j-1,k)+LH_x2(i+1,j,k)+LH_x2(i+1,j+1,k);
+            S_y = S_y*(LH_x2(i,j,k))^2;
 
             if(S_x > S_y)
-                LH_F(i-1,j-1,k) = LH_X(i,j,k);
+                LH_y(i-1,j-1,k) = LH_x1(i,j,k);
             else
-                LH_F(i-1,j-1,k) = LH_Y(i,j,k);
+                LH_y(i-1,j-1,k) = LH_x2(i,j,k);
             end
         end
     end
 end
 
-HL_F = zeros(size(HL_X,1),size(HL_X,2),3);
-HL_X = padarray(HL_X,[1 1],0,'both');
-HL_Y = padarray(HL_Y,[1 1],0,'both');
+HL_y = zeros(size(HL_x1,1),size(HL_x1,2),3);
+HL_x1 = padarray(HL_x1,[1 1],0,'both');
+HL_x2 = padarray(HL_x2,[1 1],0,'both');
 
 for k=1:3
-    for i=2:size(HL_X,1)-1
-        for j=2:size(HL_X,2)-1
-            S_x = HL_X(i,j,k)+HL_X(i-1,j-1,k)+HL_X(i-1,j,k)+HL_X(i-1,j+1,k)+HL_X(i,j-1,k)+HL_X(i,j+1,k)+HL_X(i+1,j-1,k)+HL_X(i+1,j,k)+HL_X(i+1,j+1,k);
-            S_x = S_x*(HL_X(i,j,k))^2;
+    for i=2:size(HL_x1,1)-1
+        for j=2:size(HL_x1,2)-1
+            S_x = HL_x1(i,j,k)+HL_x1(i-1,j-1,k)+HL_x1(i-1,j,k)+HL_x1(i-1,j+1,k)+HL_x1(i,j-1,k)+HL_x1(i,j+1,k)+HL_x1(i+1,j-1,k)+HL_x1(i+1,j,k)+HL_x1(i+1,j+1,k);
+            S_x = S_x*(HL_x1(i,j,k))^2;
 
-            S_y = HL_Y(i,j,k)+HL_Y(i-1,j-1,k)+HL_Y(i-1,j,k)+HL_Y(i-1,j+1,k)+HL_Y(i,j-1,k)+HL_Y(i,j+1,k)+HL_Y(i+1,j-1,k)+HL_Y(i+1,j,k)+HL_Y(i+1,j+1,k);
-            S_y = S_y*(HL_Y(i,j,k))^2;
+            S_y = HL_x2(i,j,k)+HL_x2(i-1,j-1,k)+HL_x2(i-1,j,k)+HL_x2(i-1,j+1,k)+HL_x2(i,j-1,k)+HL_x2(i,j+1,k)+HL_x2(i+1,j-1,k)+HL_x2(i+1,j,k)+HL_x2(i+1,j+1,k);
+            S_y = S_y*(HL_x2(i,j,k))^2;
 
             if(S_x > S_y)
-                HL_F(i-1,j-1,k) = HL_X(i,j,k);
+                HL_y(i-1,j-1,k) = HL_x1(i,j,k);
             else
-                HL_F(i-1,j-1,k) = HL_Y(i,j,k);
+                HL_y(i-1,j-1,k) = HL_x2(i,j,k);
             end
         end
     end
 end
 
-HH_F = zeros(size(HH_X,1),size(HH_X,2),3);
-HH_X = padarray(HH_X,[1 1],0,'both');
-HH_Y = padarray(HH_Y,[1 1],0,'both');
+HH_y = zeros(size(HH_x1,1),size(HH_x1,2),3);
+HH_x1 = padarray(HH_x1,[1 1],0,'both');
+HH_x2 = padarray(HH_x2,[1 1],0,'both');
 
 for k=1:3
-    for i=2:size(HH_X,1)-1
-        for j=2:size(HH_X,2)-1
-            S_x = HH_X(i,j,k)+HH_X(i-1,j-1,k)+HH_X(i-1,j,k)+HH_X(i-1,j+1,k)+HH_X(i,j-1,k)+HH_X(i,j+1,k)+HH_X(i+1,j-1,k)+HH_X(i+1,j,k)+HH_X(i+1,j+1,k);
-            S_x = S_x*(HH_X(i,j,k))^2;
+    for i=2:size(HH_x1,1)-1
+        for j=2:size(HH_x1,2)-1
+            S_x = HH_x1(i,j,k)+HH_x1(i-1,j-1,k)+HH_x1(i-1,j,k)+HH_x1(i-1,j+1,k)+HH_x1(i,j-1,k)+HH_x1(i,j+1,k)+HH_x1(i+1,j-1,k)+HH_x1(i+1,j,k)+HH_x1(i+1,j+1,k);
+            S_x = S_x*(HH_x1(i,j,k))^2;
 
-            S_y = HH_Y(i,j,k)+HH_Y(i-1,j-1,k)+HH_Y(i-1,j,k)+HH_Y(i-1,j+1,k)+HH_Y(i,j-1,k)+HH_Y(i,j+1,k)+HH_Y(i+1,j-1,k)+HH_Y(i+1,j,k)+HH_Y(i+1,j+1,k);
-            S_y = S_y*(HH_Y(i,j,k))^2;
+            S_y = HH_x2(i,j,k)+HH_x2(i-1,j-1,k)+HH_x2(i-1,j,k)+HH_x2(i-1,j+1,k)+HH_x2(i,j-1,k)+HH_x2(i,j+1,k)+HH_x2(i+1,j-1,k)+HH_x2(i+1,j,k)+HH_x2(i+1,j+1,k);
+            S_y = S_y*(HH_x2(i,j,k))^2;
 
             if(S_x > S_y)
-                HH_F(i-1,j-1,k) = HH_X(i,j,k);
+                HH_y(i-1,j-1,k) = HH_x1(i,j,k);
             else
-                HH_F(i-1,j-1,k) = HH_Y(i,j,k);
+                HH_y(i-1,j-1,k) = HH_x2(i,j,k);
             end
         end
     end
 end
 
 % Concatenate resulting sub-bands %
-F = [LL_F,LH_F;HL_F,HH_F];
+y = [LL_y,LH_y;HL_y,HH_y];
 
 % Perform Inverse Multiwavelet transform on RGB channels %
-F_Red = IGHM(F(:,:,1));
-F_Green = IGHM(F(:,:,2));
-F_Blue = IGHM(F(:,:,3));
+y_Red = IGHM(y(:,:,1));
+y_Green = IGHM(y(:,:,2));
+y_Blue = IGHM(y(:,:,3));
 
 % Concatenate RGB channels
-F = cat(3,F_Red,F_Green,F_Blue);
+y = cat(3,y_Red,y_Green,y_Blue);
 figure(3);
-imshow(F);
-
-
+imshow(y);
