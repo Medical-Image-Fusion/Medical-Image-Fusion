9a8f666e5c845007d8e2870a4ce0eac5210a89f8
WMirco <45242924+WMirco@users.noreply.github.com>
2019-11-16 17:02:37 -0500

    Update DTCWT_Test.m
    Conversion to values between 0-255, deleting of entropy measure, simplification of forward transform

--- DTCWT_Test.m
+++ DTCWT_Test.m
@@ -7,30 +7,39 @@ RGB_CT = ind2rgb(A_CT,map_CT);
 RGB_S = ind2rgb(A_S,map_S);
 
 %% Forward Transform of Images
-% CT-images
-impl_CT  = dddtree2('cplxdt', im2unit8(RGB_CT(:,:,1)) ,5,'dtf3');
-impl1_CT = dddtree2('cplxdt', im2unit8(RGB_CT(:,:,2)) ,5,'dtf3');
-impl2_CT = dddtree2('cplxdt',  im2unit8(RGB_CT(:,:,3)) ,5,'dtf3');
-% SPET Images
-impl_S  = dddtree2('cplxdt', im2unit8(RGB_S(:,:,1)) ,5,'dtf3');
-impl1_S = dddtree2('cplxdt', im2unit8(RGB_S(:,:,2)) ,5,'dtf3');
-impl2_S = dddtree2('cplxdt', im2unit8(RGB_S(:,:,3)) ,5,'dtf3');
+imgX1_red = im2uint8(RGB_CT(:,:,1));
+imgX1_green = im2uint8(RGB_CT(:,:,2));
+imgX1_blue = im2uint8(RGB_CT(:,:,3));
 
-%  % Highpass Coefficients
-% for j = 1:5
-%     for d = 1:3
-%         for k = 1:2
-%             for m = 1:2
-% F_Coe = (impl_CT.cfs{j}(:,:,d,k,m)+impl_S.cfs{j}(:,:,d,k,m))/2;
-% F_Coe1 = (impl1_CT.cfs{j}(:,:,d,k,m)+impl1_S.cfs{j}(:,:,d,k,m))/2;
-% F_Coe2 = (impl2_CT.cfs{j}(:,:,d,k,m)+impl2_S.cfs{j}(:,:,d,k,m))/2;
-% impl_CT.cfs{j}(:,:,d,k,m)=F_Coe;
-% impl1_CT.cfs{j}(:,:,d,k,m)=F_Coe1;
-% impl2_CT.cfs{j}(:,:,d,k,m)=F_Coe2;
-%             end
-%         end
-%     end
-% end
+imgX2_red = im2uint8(RGB_S(:,:,1));
+imgX2_green = im2uint8(RGB_S(:,:,2));
+imgX2_blue = im2uint8(RGB_S(:,:,3));
+
+% CT-images
+impl_CT  = dddtree2('cplxdt',imgX1_red,5,'dtf3');
+impl1_CT = dddtree2('cplxdt',imgX1_green,5,'dtf3');
+impl2_CT = dddtree2('cplxdt',imgX1_blue,5,'dtf3');
+% SPET Images
+impl_S  = dddtree2('cplxdt', imgX2_red ,5,'dtf3');
+impl1_S = dddtree2('cplxdt', imgX2_green,5,'dtf3');
+impl2_S = dddtree2('cplxdt', imgX2_blue ,5,'dtf3');
+
+
+%% Highpass Coefficients
+ for j = 1:5
+     for d = 1:3
+         for k = 1:2
+             for m = 1:2
+ F_Coe = (impl_CT.cfs{j}(:,:,d,k,m)+impl_S.cfs{j}(:,:,d,k,m))/2;
+ F_Coe1 = (impl1_CT.cfs{j}(:,:,d,k,m)+impl1_S.cfs{j}(:,:,d,k,m))/2;
+ F_Coe2 = (impl2_CT.cfs{j}(:,:,d,k,m)+impl2_S.cfs{j}(:,:,d,k,m))/2;
+ impl_CT.cfs{j}(:,:,d,k,m)=F_Coe;
+ impl1_CT.cfs{j}(:,:,d,k,m)=F_Coe1;
+ impl2_CT.cfs{j}(:,:,d,k,m)=F_Coe2;
+             end
+         end
+     end
+ end
 %% Lowpass Coefficients 
 a = 1
 b = 1
@@ -62,9 +71,3 @@ image(:,:,3) = dtImage3;
 %% show
 figure
 imshow(image)
-
-%% measure
-e = entropy(image)
-e1 = entropy(RGB_CT)
-e2 = entropy(RGB_S)
-
